969a11660fed75430639fa40449f27de496acb76
niltinfilho <niltonfilho115@gmail.com>
2025-11-10 13:11:10 -0300

    Trabalhando com Datas e Horas

--- todo_list/lib/widgets/todo_list_item.dart
+++ todo_list/lib/widgets/todo_list_item.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:todo_list/models/todo.dart';
 
 class TodoListItem extends StatelessWidget {
-  const TodoListItem({super.key, required this.title});
+  const TodoListItem({super.key, required this.todo});
 
-  final String title;
+  final Todo todo;
 
   @override
   Widget build(BuildContext context) {
@@ -19,11 +21,11 @@ class TodoListItem extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            '20/11/2020',
+            DateFormat('dd/MM/yyyy - HH:mm').format(todo.dateTime),
             style: TextStyle(fontSize: 12),
           ),
           Text(
-            title,
+            todo.title,
             style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
           ),
         ],
