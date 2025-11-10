cf5b798009e3fbc4383f91c6402a8b771769987c
niltinfilho <niltonfilho115@gmail.com>
2025-11-10 10:18:10 -0300

    personalizando campo de texto

--- todo_list/lib/pages/todo_list_page.dart
+++ todo_list/lib/pages/todo_list_page.dart
@@ -5,8 +5,29 @@ class TodoListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.deepPurpleAccent,
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: TextField(
+            decoration: InputDecoration(
+              labelText: 'E-mail',
+              hintText: 'exemplo@exemplo.com',
+              border: OutlineInputBorder(),
+              errorText: 'Campo obrigatório',
+              prefixText: 'R\$ ',
+              suffixText: 'cm',
+            ),
+            obscureText: true,
+            keyboardType: TextInputType.numberWithOptions(),
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.purple,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
