224774c58d222c1297e93c3086253c7a1629fb10
niltinfilho <niltonfilho115@gmail.com>
2025-11-10 13:30:33 -0300

    Package flutter_slidable

--- todo_list/lib/widgets/todo_list_item.dart
+++ todo_list/lib/widgets/todo_list_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 import 'package:todo_list/models/todo.dart';
 
@@ -9,26 +10,39 @@ class TodoListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: Colors.grey[200],
-      ),
-
-      padding: EdgeInsets.all(16),
-      margin: EdgeInsets.symmetric(vertical: 4),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+    return Slidable(
+      endActionPane: ActionPane(
+        motion: const StretchMotion(),
+        extentRatio: 0.2,
         children: [
-          Text(
-            DateFormat('dd/MM/yyyy - HH:mm').format(todo.dateTime),
-            style: TextStyle(fontSize: 12),
-          ),
-          Text(
-            todo.title,
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+          SlidableAction(
+            borderRadius: BorderRadius.circular(4),
+            backgroundColor: Colors.red,
+            icon: Icons.delete,
+            onPressed: (context) {},
           ),
         ],
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.grey[200],
+        ),
+        padding: EdgeInsets.all(16),
+        margin: EdgeInsets.symmetric(vertical: 4),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              DateFormat('dd/MM/yyyy - HH:mm').format(todo.dateTime),
+              style: TextStyle(fontSize: 12),
+            ),
+            Text(
+              todo.title,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+            ),
+          ],
+        ),
       ),
     );
   }
